a643dc634d2d89017ae31a23b6ae9e10ced35002
Rushikesh Deshmukh <58228830+rushikdm@users.noreply.github.com>
2020-01-18 20:30:27 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <assert.h>
+
+//#include "rushi_utils.h"
+//#include "rushi_dbg.h"
+
+using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+typedef pair<int, int> pr_ii;
+
+typedef vector<int>       vec_i;
+typedef vector<long long> vec_ll;
+typedef vector<float>     vec_f;
+typedef vector<double>    vec_d;
+typedef vector<char>      vec_c;
+typedef vector<string>    vec_s;
+
+#define FOR(i, b, e)  for(int i=(b); i < (e); ++i)
+#define REP(i, n)     for(int i = 0; i < (n); ++i)
+
+#define sort_all(c) sort(c.begin(), c.end());
+#define PB push_back
+#define F first
+#define S second
+
+class CodeChef {
+	public:
+	CodeChef() {
+	}
+	
+	void solve() {
+	}
+
+	~CodeChef() {
+	}
+};
+		
+int main() {
+	FASTIO;
+	int t;
+	cin >> t;
+	while(t--) {
+	CodeChef cc;
+	cc.solve();
+	}
+	
+	return 0;
+}
+
